73453fbf36ea35d7c03a14ed57c117afc13c6721
Brictone <briang@bg3d.com>
2023-03-03 10:15:58 -0800

    Collab Viewer
    Switched project to the Collab viewer, this has Multiplayer all setup for VR, so should be a good base to start from.
    https://docs.unrealengine.com/4.27/en-US/Resources/Templates/CollabViewer/

--- Source/VRMP_1/VRMP_1.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VRMP_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRMP_1, "VRMP_1" );
