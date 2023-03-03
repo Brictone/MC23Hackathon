7c04f00e3aeb401652f5ae420a83834b9818f917
Brictone <briang@bg3d.com>
2023-03-03 09:51:58 -0800

    Initial Commit
    New MP VR Template

---
+++ Source/VRMP_1/VRMP_1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRMP_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRMP_1, "VRMP_1" );
